e787360fef9e34564bdc1c1a4208451f77ef7fd6
Henrik Bengtsson <hb@aroma-project.org>
2012-11-06 08:46:47 +0000

    Version: 0.30.0 [2012-11-05] o GENERALIZATION: Now bootstrapTCNandDHByRegion() works for more   "flavors", e.g the default ('tcn') used by segmentByNonPairedPSCBS().
    Version: 0.29.9 [2012-11-05]
    o DOCUMENTATION FIX: example(segmentByNonPairedPSCBS) was for
      the paired case.
    o CRAN POLICY: Further speed up of examples such that they run faster
      with R CMD check.
    
    
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/abo/pkg/PSCBS@349 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/prememoize.R
+++ R/prememoize.R
@@ -1,10 +1,9 @@
 # CRAN POLICY: Add precalculated memoization files to the R.cache 
-# directory, iff running R CMD check.  The reason for doing this 
+# directory, unless running interactively.  The reason for doing this 
 # is solely to make segmentBy[Non]PairedPSCBS examples to run faster
 # on R CMD check but not having to create these memoized files.
 .prememoize <- function() {
-  # Running 'R CMD check'?
-  if (queryRCmdCheck() != "notRunning") {
+  if (!interactive()) {
     require("R.cache") || throw("Package not loaded: R.cache");
     path <- "PSCBS/segmentByCBS/sbdry"
     pathS <- system.file("misc/_Rcache", path, package="PSCBS");
