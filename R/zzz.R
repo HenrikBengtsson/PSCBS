d5f4576bd0e7c9d411b4b7896594291d5c6460d3
hb <hb@aroma-project.org>
2016-11-10 09:07:31 -0800

    HTTP -> HTTPS

--- R/zzz.R
+++ R/zzz.R
@@ -33,7 +33,7 @@
 
   # Inform user if DNAcopy is missing
   if (!isPackageInstalled("DNAcopy")) {
-    msg <- "The Bioconductor package 'DNAcopy' is not installed. Please see http://www.bioconductor.org/ on how to install it, or try calling PSCBS::installDNAcopy().";
+    msg <- "The Bioconductor package 'DNAcopy' is not installed. Please see https://www.bioconductor.org/ on how to install it, or try calling PSCBS::installDNAcopy().";
     hrule <- paste(rep("*", times=getOption("width", 80L)-1L), collapse="");
     packageStartupMessage(sprintf("%s\nNOTE: %s\n%s", hrule, msg, hrule));
   }
