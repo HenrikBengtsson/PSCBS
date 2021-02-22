60532af297581a764c771cc12d72a3e376c81a7d
Henrik Bengtsson <hb@aroma-project.org>
2021-02-22 12:53:35 -0800

    CLEANUP: append(x, y) for AbstractCBS is now defunct in favor of c()

--- R/AbstractCBS.RESTRUCT.R
+++ R/AbstractCBS.RESTRUCT.R
@@ -89,12 +89,13 @@
 # \seealso{
 #   @seeclass
 # }
+#
+# @keyword internal
 #*/###########################################################################
 setMethodS3("append", "AbstractCBS", function(x, other, addSplit = TRUE, ...) {
   new <- if (addSplit) "c(x, other, addSplit = TRUE)" else "c(x, other)"
-  old <- sprintf("append.%s()", class(x)[1])
-  .Deprecated(new = new, old = old)
-  c(x, other, addSplit = addSplit)  
+  msg <- sprintf("append() for %s is defunct. Use %s instead.", class(x)[1], new)
+  .Defunct(msg = msg, package = .packageName)
 })
 
 
